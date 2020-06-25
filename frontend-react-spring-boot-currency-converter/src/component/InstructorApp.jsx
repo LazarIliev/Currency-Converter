f58f9cd501bd6cfd15a9e40f7a69947895ffe621
Lazar Iliev <lazarilievplovdiv@mail.bg>
2020-06-25 12:09:48 +0300

    package.json dependencies added axios, react-router-dom, formik and ListCurrenciesComponent created

--- frontend-react-spring-boot-currency-converter/src/component/InstructorApp.jsx
+++ frontend-react-spring-boot-currency-converter/src/component/InstructorApp.jsx
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import ListCurrenciesComponent from './ListCurrenciesComponent';
 
 class InstructorApp extends Component {
     render() {
-        return (
+        return (<>
             <h1> Instructor Application!</h1>
+            <ListCurrenciesComponent/>
+            </>
         )
     }
 }
